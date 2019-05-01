7880f75a54fedb02d0b59b45688b5826305c2cc9
c1294103 <lcsantos1@stefanini.com>
2019-05-01 16:53:04 -0300

    criando component menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OlaMundoComponent,
     PainelSimplesComponent,
-    BarraNavegacaoComponent
+    BarraNavegacaoComponent,
+    MenuLateralComponent
   ],
   imports: [
     BrowserModule
