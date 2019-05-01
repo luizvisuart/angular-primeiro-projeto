3b592ea5a6627ea4b5a8dbd37c09d13ebd8227d4
c1294103 <lcsantos1@stefanini.com>
2019-05-01 17:17:33 -0300

    usando dados de um objeto json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-latera
     OlaMundoComponent,
     PainelSimplesComponent,
     BarraNavegacaoComponent,
-    MenuLateralComponent
+    MenuLateralComponent,
+    ResumoComponent
   ],
   imports: [
     BrowserModule
