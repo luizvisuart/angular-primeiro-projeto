9498df45215757f9b3f05b1c5e0bb6eb747179df
c1294103 <lcsantos1@stefanini.com>
2019-05-01 12:57:33 -0300

    Ajustando o projeto

--- src/app/painel-simples/painel-simples.component.ts
+++ src/app/painel-simples/painel-simples.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'app-painel-simples',
   templateUrl: './painel-simples.component.html',
   styleUrls: ['./painel-simples.component.css']
 })
-export class PainelSimplesComponent implements OnInit {
+export class PainelSimplesComponent {
+  @Input() titulo = "Titulo do Painel";
+  @Input() col = 6;
+  @Input() tipo = "info";
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"http://www.devup.com.br/php/api-dashboard/api/"
 };
+  
